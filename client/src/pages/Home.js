dd2a157021dff31a876676db9009f410fc3ba940
Mark Andersen <10101087+MarkAndersen@users.noreply.github.com>
2021-12-22 12:22:02 -0500

    background image tweaks

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,26 @@
 import React from "react";
-import { Box, Button, ButtonGroup, Paper,Container } from "@mui/material";
+import { Box, Button, ButtonGroup, Container, Paper } from "@mui/material";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import useSound from "use-sound";
 import picture from "../assets/wav-pool-sm.jpg";
 import brrtt from "../assets/brrtt.wav";
 import train from "../assets/train.wav";
+import { height } from "@mui/system";
 
-const styles = {
-  image: {
-    Height: "100vh",
-    width: "100vw"
-  }
-}
+// const styles = {
+//   image: {
+//     // Height: "99vh",
+//     Width: "100%"
+//   },
+
+//   pictureBox: {
+//     backgroundImage: `url(${picture})`,
+//     height: 500,
+//     width: "100%"
+
+//   }
+// }
 
 export default function Home() {
     const [toot] = useSound(brrtt);
@@ -21,13 +29,17 @@ export default function Home() {
   return (
     <React.Fragment>
       <Navbar />
-          <img alt="hot tub" src={picture} style={styles.image}></img>
-          <br></br>
-        <ButtonGroup variant="text" aria-label="text button group">
-          <Button onClick={toot}>One</Button>
-          <Button onClick={whistle}>Two</Button>
-          <Button>Three</Button>
-        </ButtonGroup> 
+      <Box sx={{ width:"100%", height: 1500, backgroundImage:`url(${picture})`}}>
+          {/* <img alt="hot tub" src={picture} style={styles.image}></img>
+          <br></br> */}
+        <Container>
+          <ButtonGroup variant="contained" aria-label="text button group">
+            <Button onClick={toot}>One</Button>
+            <Button onClick={whistle}>Two</Button>
+            <Button>Three</Button>
+          </ButtonGroup>
+        </Container>
+      </Box>
       <Footer />
       
     </React.Fragment>
