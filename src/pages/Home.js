747a0d343e4c5e672d743e3aa05bd1fc3dbc7a9b
Mark Andersen <10101087+MarkAndersen@users.noreply.github.com>
2022-01-11 18:54:36 -0500

    background images for each button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,13 +14,19 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import useSound from "use-sound";
 import picture from "../assets/wav-pool-sm.jpg";
+import btnPicture from "../assets/seal-seal.jpg";
+import btnPicture2 from "../assets/looney.gif";
+import btnPicture3 from "../assets/bird.jpg";
+import btnPicture4 from "../assets/train.jpg";
+import btnPicture5 from "../assets/toot.jpg";
 import brrtt from "../assets/brrtt.wav";
 import train from "../assets/train.wav";
 import bjird from "../assets/bjird.wav";
 import bobbo from "../assets/bobbo1.wav";
 import bobbo2 from "../assets/bobbo2.wav";
 import bobbo3 from "../assets/bobbo3.wav";
-import tunes from "../assets/loontunes.wav"
+import tunes from "../assets/loontunes.wav";
+import kiss from "../assets/seal.wav";
 
 export default function Home() {
   const theme = createTheme({
@@ -41,112 +47,203 @@ export default function Home() {
   const [bob2] = useSound(bobbo2);
   const [bob3] = useSound(bobbo3);
   const [looney] = useSound(tunes);
+  const [seal] = useSound(kiss);
 
   return (
     <React.Fragment>
-      <Container maxWidth="false" disableGutters>
+      <Container
+        sx={{ backgroundColor: "Blue" }}
+        maxWidth="false"
+        disableGutters
+      >
         <Navbar />
         <Box
           sx={{
             maxWidth: "100vw",
-            maxHeight: "100vh",
+            minHeight: "100vh",
             backgroundPosition: "center",
             backgroundSize: "cover",
-            backgroundImage: `url(${picture})`, 
+            backgroundImage: `url(${picture})`,
           }}
         >
-          {/* <img alt="hot tub" src={picture} style={styles.image}></img>
-          <br></br> */}
-          {/* <br></br>
-          <br></br>
-          <br></br> */}
-          {/* <br></br>
-          <br></br>
-          <br></br> */}
-          <Grid container spacing={1}>
-            <Grid item xs={12}>
-              <ThemeProvider theme={theme}>
+          <Grid>
+            <ThemeProvider theme={theme}>
+              <Grid item>
                 <ButtonGroup
                   size="large"
                   fullWidth="true"
                   variant="outlined"
                   aria-label="text button group"
                 >
-                  <Button sx={{ m: 4 }} onClick={toot}>
-                    <Typography variant="h1">&#128169;</Typography>
-                  </Button>
-                  <Button sx={{ m: 4 }} onClick={whistle}>
-                    <Typography variant="h1">&#128646;</Typography>
-                  </Button>
-                  <Button sx={{ m: 4 }} onClick={bird}>
-                    <Typography variant="h1">&#x1F426;</Typography>
-                  </Button>
+                  <Button
+                  
+                    sx={{
+                      m: 2,
+                      backgroundImage: `url(${btnPicture5})`,
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                      opacity: "60%",
+                      minHeight: "20vh",
+                    }}
+                    onClick={toot}
+                  ></Button>
+                  <Button
+                  
+                    sx={{
+                      m: 2,
+                      backgroundImage: `url(${btnPicture4})`,
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                      opacity: "60%",
+                      minHeight: "20vh",
+                    }}
+                    onClick={whistle}
+                  ></Button>
+                  <Button
+                  
+                    sx={{
+                      m: 2,
+                      backgroundImage: `url(${btnPicture3})`,
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                      opacity: "60%",
+                      minHeight: "20vh",
+                    }}
+                    onClick={bird}
+                  ></Button>
                 </ButtonGroup>
-              </ThemeProvider>
-            </Grid>
-            <Grid item xs={12}>
-              <ThemeProvider theme={theme}>
+              </Grid>
+              <Grid item>
                 <ButtonGroup
                   size="large"
                   fullWidth="true"
                   variant="outlined"
                   aria-label="text button group"
                 >
-                  <Button sx={{ m: 4 }} onClick={bob1}>
-                    <Typography variant="h1">&#128054;</Typography>
-                  </Button>
-                  <Button sx={{ m: 4 }} onClick={bob2}>
-                    <Typography variant="h1">&#128021;</Typography>
-                  </Button>
-                  <Button sx={{ m: 4 }} onClick={bob3}>
-                    <Typography variant="h1">&#128054;</Typography>
-                  </Button>
+                  <Button
+                    sx={{
+                      m: 2,
+                      backgroundImage: `url(${btnPicture3})`,
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                      opacity: "60%",
+                      minHeight: "20vh",
+                    }}
+                    onClick={bob1}
+                  ></Button>
+                  <Button
+                    sx={{
+                      m: 2,
+                      backgroundImage: `url(${btnPicture3})`,
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                      opacity: "60%",
+                      minHeight: "20vh",
+                    }}
+                    onClick={bob2}
+                  ></Button>
+                  <Button
+                    sx={{
+                      m: 2,
+                      backgroundImage: `url(${btnPicture3})`,
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                      opacity: "60%",
+                      minHeight: "20vh",
+                    }}
+                    onClick={bob3}
+                  ></Button>
                 </ButtonGroup>
-              </ThemeProvider>
-            </Grid>
-            <Grid item xs={12}>
-              <ThemeProvider theme={theme}>
+              </Grid>
+              <Grid item>
                 <ButtonGroup
                   size="large"
                   fullWidth="true"
                   variant="outlined"
                   aria-label="text button group"
                 >
-                  <Button sx={{ m: 4 }} onClick={bob1}>
-                    <Typography variant="h1">&#128054;</Typography>
-                  </Button>
-                  <Button sx={{ m: 4 }} onClick={bob2}>
-                    <Typography variant="h1">&#128021;</Typography>
-                  </Button>
-                  <Button sx={{ m: 4 }} onClick={bob3}>
-                    <Typography variant="h1">&#128054;</Typography>
+                  <Button
+                    sx={{
+                      m: 2,
+                      backgroundImage: `url(${btnPicture2})`,
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                      opacity: "60%",
+                    }}
+                    onClick={looney}
+                  ></Button>
+                  <Button
+                    sx={{
+                      m: 2,
+                      backgroundImage: `url(${btnPicture})`,
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                      opacity: "60%",
+                    }}
+                    onClick={seal}
+                  ></Button>
+                  <Button
+                    sx={{
+                      m: 2,
+                      backgroundImage: `url(${btnPicture3})`,
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                      opacity: "60%",
+                      minHeight: "20vh",
+                    }}
+                    onClick={bob3}
+                  >
                   </Button>
                 </ButtonGroup>
-              </ThemeProvider>
-            </Grid>
-            <Grid item xs={12}>
-              <ThemeProvider theme={theme}>
+              </Grid>
+              <Grid item xs={12}>
                 <ButtonGroup
                   size="large"
                   fullWidth="true"
                   variant="outlined"
                   aria-label="text button group"
                 >
-                  <Button sx={{ m: 4 }} onClick={bob1}>
-                    <Typography variant="h1">&#128054;</Typography>
+                  <Button
+                    sx={{
+                      m: 2,
+                      backgroundImage: `url(${btnPicture3})`,
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                      opacity: "60%",
+                      minHeight: "20vh",
+                    }}
+                    onClick={bob1}
+                  >
                   </Button>
-                  <Button sx={{ m: 4 }} onClick={bob2}>
-                    <Typography variant="h1">&#128021;</Typography>
+                  <Button
+                    sx={{
+                      m: 2,
+                      backgroundImage: `url(${btnPicture3})`,
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                      opacity: "60%",
+                      minHeight: "20vh",
+                    }}
+                    onClick={bob2}
+                  >
                   </Button>
-                  <Button sx={{ m: 4 }} onClick={bob3}>
-                    <Typography variant="h1">&#128054;</Typography>
+                  <Button
+                    sx={{
+                      m: 2,
+                      backgroundImage: `url(${btnPicture3})`,
+                      backgroundSize: "cover",
+                      backgroundPosition: "center",
+                      opacity: "60%",
+                      minHeight: "20vh",
+                    }}
+                    onClick={bob3}
+                  >
                   </Button>
                 </ButtonGroup>
-              </ThemeProvider>
-            </Grid>
+              </Grid>
+            </ThemeProvider>
           </Grid>
         </Box>
-
         <Footer />
       </Container>
     </React.Fragment>
